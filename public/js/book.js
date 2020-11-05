7cefe7ebc9f1b7c7adb1b33cafdfe6a47095440e
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2020-11-05 13:33:20 -0500

    Partial ratings show

--- public/js/book.js
+++ public/js/book.js
@@ -1,18 +1,28 @@
 const id = Number(window.location.pathname.split('/')[2]);
 let sent = true;
+let bookData;
+let reviews;
+
 const cover = document.getElementById('cover');
 const title = document.getElementById('title');
 const summary = document.getElementById('summary');
 const authors = document.getElementById('author');
 const series = document.getElementById('series');
 const reviewContent = document.getElementById('reviews')
+const reviewCount = document.querySelector('.review-count');
+const avgRating = document.querySelector('.avg-rating');
 const manageShelves = document.querySelector('.select-shelves-placeholder');
 
 async function getBook() {
     const res = await fetch(`/api/books/${id}`);
     const data = await res.json();
-    const book = data.book;
-    const userId = data.userId;
+    bookData = data;
+    return bookData;
+};
+
+function populateBookContent() {
+    const { book } = bookData;
+    const userId = bookData.userId;
     const author = book.Authors[0];
     const seriesData = book.Series;
     const genres = book.Genres;
@@ -23,12 +33,15 @@ async function getBook() {
     summary.innerHTML = book.summary;
     authors.innerHTML = `By ${author.firstName} ${author.lastName}`;
     series.innerHTML = `${seriesData.name} Series`;
-};
+}
 
 async function getReviews() {
     const res = await fetch(`/api/books/${id}/reviews`);
     const data = await res.json();
-    const reviews = data.reviews;
+    reviews = data.reviews;
+};
+
+function populateReviewContent() {
     reviews.forEach(review => {
         const revContainer = document.createElement('tr');
         const rev = document.createElement('td');
@@ -44,7 +57,47 @@ async function getReviews() {
         revContainer.appendChild(userInfo);
     });
 
-};
+    if (reviews.length === 1) {
+        reviewCount.innerHTML = '1 review';
+    } else if (!reviews.length) {
+        reviewCount.innerHTML = 'This book currently has no reviews';
+    } else {
+        reviewCount.innerHTML = `${reviews.length} reviews`;
+    }
+    let average = getAvgRating(reviews);
+    avgRating.innerHTML = average;
+
+    let quotient = Math.floor(average);
+    let remainder = average % quotient;
+
+    for (let i = 1; i <= quotient; i++) {
+        document.querySelector(`#star-path-${i}`).classList.add('filled-star');
+    }
+
+    if (remainder > 0) {
+        let innerSVG = `
+        <defs>
+        <linearGradient id="grad">
+        <stop offset="50%" stop-color="yellow"/>
+        <stop offset="50%" stop-color="grey" stop-opacity="1" />
+    </linearGradient>
+    </defs>
+    <path id="star-path-4" d="M54,5 86,105 1,43H107L22,105" fill="url(#grad)"></path>`;
+
+        document.querySelector(`#star-view-${quotient + 1}`).innerHTML = innerSVG;
+    }
+}
+
+function getAvgRating(reviews) {
+    // Filter out reviews with no rating
+    let ratings = reviews.map((review) => review.rating).filter((rating) => rating >= 1);
+    if (ratings.length) {
+        let avgRating = ratings.reduce((accum, val) => accum += val) / ratings.length;
+        return avgRating.toFixed(1);
+    } else {
+        return "This book has no ratings";
+    }
+}
 
 const populateShelves = async () => {
     const res = await fetch('/api/bookshelves');
@@ -75,46 +128,49 @@ const populateShelves = async () => {
     document.querySelector('.shelves__dropdown--created-shelves').innerHTML = shelfStr;
 }
 
-manageShelves.addEventListener('click', async event => {
-    const target = event.target;
-    console.log(target.classList.contains('bookshelves-text'));
-    if (!target.classList.contains('select-shelves-placeholder') && !target.classList.contains('bookshelves-text') && !target.classList.contains('shelf-arrow-placeholder')) {
-        return;
-    }
-    document.querySelector('.shelve-list-container').classList.toggle('hidden');
-    const formData = new FormData(document.querySelector('form'));
-    if (sent) {
-        sent = false;
-    } else {
-        sent = true;
-        const text = document.querySelector('.bookshelves-text');
-        text.innerHTML = 'Saving...';
-        text.classList.add('saving');
-        document.querySelector('.shelf-arrow-placeholder').innerHTML = '';
-        const body = {
-        };
-        for (let key of formData.keys()) {
-            body[key] = formData.get(key);
-        }
-
-        const res = await fetch(`/api/books/${id}`, {
-            method: 'POST',
-            body: JSON.stringify(body),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (res.ok) {
-            window.location.pathname = '/my-books';
-        }
-    }
-})
-
-getBook();
-getReviews();
-populateShelves();
+// getReviews();
+// populateShelves();
 
 document.addEventListener('DOMContentLoaded', async event => {
+    await getBook();
+    await getReviews();
 
+    populateBookContent();
+    populateReviewContent();
+
+    manageShelves.addEventListener('click', async event => {
+        const target = event.target;
+        console.log(target.classList.contains('bookshelves-text'));
+        if (!target.classList.contains('select-shelves-placeholder') && !target.classList.contains('bookshelves-text') && !target.classList.contains('shelf-arrow-placeholder')) {
+            return;
+        }
+        document.querySelector('.shelve-list-container').classList.toggle('hidden');
+        const formData = new FormData(document.querySelector('form'));
+        if (sent) {
+            sent = false;
+        } else {
+            sent = true;
+            const text = document.querySelector('.bookshelves-text');
+            text.innerHTML = 'Saving...';
+            text.classList.add('saving');
+            document.querySelector('.shelf-arrow-placeholder').innerHTML = '';
+            const body = {
+            };
+            for (let key of formData.keys()) {
+                body[key] = formData.get(key);
+            }
+
+            const res = await fetch(`/api/books/${id}`, {
+                method: 'POST',
+                body: JSON.stringify(body),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (res.ok) {
+                window.location.pathname = '/my-books';
+            }
+        }
+    })
 })
